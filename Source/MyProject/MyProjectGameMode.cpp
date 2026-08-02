3b44f972770e90a88db19c70eae11da0b7cc1303
Pavel <ipgritsay@yandex.ru>
2026-08-02 22:00:46 +0500

    fix: revert DefaultPawnClass path to ThirdPerson root

--- Source/MyProject/MyProjectGameMode.cpp
+++ Source/MyProject/MyProjectGameMode.cpp
@@ -7,7 +7,7 @@
 AMyProjectGameMode::AMyProjectGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPartyContent/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
